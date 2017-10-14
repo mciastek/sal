606344791b76bf5fb2f52731802b131872d35180
Mirosław Ciastek <mirek@swingdev.io>
2017-10-14 23:59:46 +0200

    Add basic config for puppeteer.

---
+++ test/config/browser.js
@@ -0,0 +1,51 @@
+// eslint-disable-next-line max-len
+// see: https://medium.com/@ivanmontiel/using-that-headless-chrome-youve-been-hearing-about-543a8cc07af5
+
+const puppeteer = require('puppeteer');
+
+/**
+ * This is a thin wrapper so that we use a singleton of
+ * the browser that puppeteer creates
+ */
+class Browser {
+  setUp(done) {
+    const puppeteerOpts = this.options && this.options.puppeteer ?
+      this.options.puppeteer :
+      {};
+    puppeteer.launch(puppeteerOpts).then(async (b) => {
+      this.setBrowser(b);
+      done();
+    });
+  }
+
+  setBrowser(b) {
+    this.browser = b;
+    const oldNewPage = this.browser.newPage.bind(this.browser);
+
+    this.browser.newPage = async function () {
+      const page = await oldNewPage();
+      this.lastPage = page;
+      return page;
+    };
+  }
+
+  setOptions(opts) {
+    this.options = opts;
+  }
+
+  inBrowser(promise) {
+    return (done) => {
+      promise(this.browser, this.options)
+        .then(() => done()).catch(done);
+    };
+  }
+}
+/*
+ * Create a new browser and use a proxy to pass
+ * any puppeteer calls to the inner browser
+ */
+module.exports = new Proxy(new Browser(), {
+  get: (target, name) => (
+    (name in target) ? target[name].bind(target) : target.browser[name]
+  ),
+});
---
+++ test/config/options.js
@@ -0,0 +1,5 @@
+const path = require('path');
+
+module.exports = {
+  rootUrl: path.resolve(__dirname, 'test', 'fixtures'),
+};
